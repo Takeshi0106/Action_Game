d185f3b00f75b7cb8e7ea6b34d52faa37178326d
Takeshi <matsuda.takeshi.30692@gmail.com>
2025-07-23 13:40:06 +0900

    DirectXにplatformWindowsSystemクラスと同じ前方定義を追加して、ウィンドウハンドルを受け取れるように

--- 3DAction_Project/DirectX.cpp
+++ 3DAction_Project/DirectX.cpp
@@ -1,6 +1,30 @@
 ﻿#include "DirectX.h"
+// #include <dxgi.h>
 #include <d3d11.h>
 #include <wrl/client.h> // マイクロソフトが提供するスマートポインタ
+#include <Windows.h>
+
+// =======================================
+// 前方定義したラッパー構造体の中身
+// =======================================
+struct DX_WINDOWHANDLE {
+private: // 直接アクセス不可
+	HWND hWnd; // ウィンドウハンドル
+
+public:
+	// コンストラクタ
+	DX_WINDOWHANDLE(const HWND& _hWnd) : hWnd(_hWnd) {}
+	// 代入演算子オーバーロード
+	DX_WINDOWHANDLE& operator=(const HWND& _hWnd)
+	{
+		hWnd = _hWnd;
+		return *this;
+	}
+	// 暗黙的に変換を禁止にする
+	explicit operator HWND() const { return hWnd; }
+	// ラップしているHWNDを返す
+	HWND Get() const { return hWnd; }
+};
 
 
 // =======================================
@@ -9,10 +33,16 @@
 
 namespace DirectX11 {
 
+	namespace {
+		// 使用しているモニターなどを取得する変数
+		// Microsoft::WRL::ComPtr<IDXGIOutput> pOutput = null;
+		// Microsoft::WRL::ComPtr<ID3D11Device> pDevice = null;
+	}
+
 	// =====================================================
 	// 初期化処理
 	// =====================================================
-	void Init(uint16_t Width, uint16_t Height)
+	void Init(uint16_t Width, uint16_t Height, DX_WINDOWHANDLE windowHandle)
 	{
 		unsigned int createDeviceFlags = 0; // デバイス作成用のフラグ
 #if defined(DEBUG) || defined(_DEBUG)
@@ -31,21 +61,37 @@ namespace DirectX11 {
 		// 機能のレベルの優先度を作成 ------------------------------------------------------------
 		D3D_FEATURE_LEVEL featureLevels[] = {
 			D3D_FEATURE_LEVEL_11_0, // DirectX11対応 (コンピュートシェーダーなどが使用可能)
-			
+
 			// もし対応できるようにする場合シェーダーを別に書かないといけない
 			// D3D_FEATURE_LEVEL_10_1, コンピュートシェーダーなどは使用できない
 			// D3D_FEATURE_LEVEL_10_0,
 		};
 		unsigned int numFeatureLevels = sizeof(featureLevels) / sizeof(D3D_FEATURE_LEVEL); // ループする回数を取得する
 
+		/*
+		// 使用しているモニターなどのリフレッシュレートを取得する
+		DXGI_MODE_DESC desiredDesc = {};
+		desiredDesc.Width = 1920;
+		desiredDesc.Height = 1080;
+		desiredDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+		desiredDesc.RefreshRate.Numerator = 0;
+		desiredDesc.RefreshRate.Denominator = 1;
+		MF
+		DXGI_MODE_DESC closestDesc = {};
+		pOutput->FindClosestMatchingMode(&desiredDesc, &closestDesc, pDevice.Get());
+		*/
+
 		// スワップチェイン作成 -------------------------------------------------------------------
 		DXGI_SWAP_CHAIN_DESC swapChainDesc = {}; // C++のためこっちで初期化
 		// ZeroMemory(&swapChainDesc, sizeof(DXGI_SWAP_CHAIN_DESC));  // 全てに0を代入して、初期化する
 		swapChainDesc.BufferCount = 2;                                // ダブルバッファ
-		swapChainDesc.BufferDesc.Width = Width;                 // 画面の縦幅 今はウィンドウサイズと同じにしている
-		swapChainDesc.BufferDesc.Height = Height;               // 画面の横幅 今はウィンドウサイズと同じにしている
+		swapChainDesc.BufferDesc.Width = Width;                       // 画面の縦幅 今はウィンドウサイズと同じにしている
+		swapChainDesc.BufferDesc.Height = Height;                     // 画面の横幅 今はウィンドウサイズと同じにしている
 		swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; // RGBA 各8ビット 0.0~1.0に正規化　＊一般的でどの環境でも動きやすい
-		swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;          // 
+		swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;           // リフレッシュレート　分母 (0の場合,OSに任せる)
+		swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;         // リフレッシュレート　分子 (*ウィンドウモードの時は適用されない)
+		swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT | DXGI_USAGE_SHADER_INPUT; // バックバッファの使用用途
+		swapChainDesc.OutputWindow = windowHandle.Get();
 	}
 
 
--- 3DAction_Project/DirectX.h
+++ 3DAction_Project/DirectX.h
@@ -2,8 +2,14 @@
 #include <cstdint>
 
 // ==============================================
+// 前方定義
+// ==============================================
+// ウィンドウハンドルの前方宣言
+struct DX_WINDOWHANDLE;                   // DierctXの初期化で使用するHWNDのラップ構造体
+
+// ==============================================
 // DirectXの関数
 // ==============================================
 namespace DirectX11 {
-	void Init(uint16_t Width, uint16_t Height);
+	void Init(uint16_t Width, uint16_t Height, DX_WINDOWHANDLE windowHandle);
 }
