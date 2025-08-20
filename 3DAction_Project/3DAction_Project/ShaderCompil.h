6fefbb96d639b51b242b94962b1c8ad3444f63e7
Takeshi <matsuda.takeshi.30692@gmail.com>
2025-08-20 09:59:00 +0900

    改行追加

--- 3DAction_Project/3DAction_Project/ShaderCompil.h
+++ 3DAction_Project/3DAction_Project/ShaderCompil.h
@@ -24,5 +24,6 @@ const std::string kCompileExtension = ".cso"; // コンパイル後の拡張子
 // シェーダーをコンパイルして外部ファイルに書き出し引き数のblobにバイナリデータを入れる
 bool OutputCompileShader(const std::filesystem::path kFilePath, const std::filesystem::path name,
 	const std::string entryPoint, const std::string shaderTypeModel, ID3DBlob** blob);
+
 // パスから.csoを読み込んでblobにバイナリーデータを入れる関数
 bool LoadCompiledShader(const std::filesystem::path& csoPath, ID3DBlob** blob);
