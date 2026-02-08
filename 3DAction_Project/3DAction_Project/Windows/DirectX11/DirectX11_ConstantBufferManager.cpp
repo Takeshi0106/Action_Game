e54ebc13bca06db6c9ff74d4ce17ef4a14149314
MatsudaT <matsudat.30692@gmail.com>
2026-02-08 20:42:00 +0900

    定数バッファマネージャーにデバッグ情報追加

--- 3DAction_Project/3DAction_Project/Windows/DirectX11/DirectX11_ConstantBufferManager.cpp
+++ 3DAction_Project/3DAction_Project/Windows/DirectX11/DirectX11_ConstantBufferManager.cpp
@@ -20,12 +20,31 @@ Handle DirectX11_ConstantBufferManager::ConstantBufferCreateOnGet(
 	// すでに存在しているか確認
 	if (m_ConstantBuffers.Exists(_cbName)) 
 	{
+		// 取得
+		Handle handle = m_ConstantBuffers.GetHandle(_cbName);
+
+#if defined(DEBUG) || defined(_DEBUG)
+		// デバッグ情報作成
+		DirectX11_DebugInfo debugInfo;
+		debugInfo.BufferSize = (uint32_t)_desc.ByteWidth;
+		debugInfo.Usage = _desc.Usage;
+		debugInfo.CPUAccessFlags = (uint32_t)_desc.CPUAccessFlags;
+
+		// 同一確認
+		if (!m_ConstantBuffers.GetData(handle)->IsSame(debugInfo))
+		{
+			WarningLog::OutputToConsole(u8"定数バッファ : " +
+				_cbName.GetString() + u8" は異なる設定で作成されようとしました");
+			return Handle();
+		}
+#endif
+
 		// ログ出力
 		WarningLog::OutputToConsole(u8"定数バッファ : " +
 			_cbName.GetString() + u8" はすでに存在しています");
 
 		// ハンドルを返す
-		return m_ConstantBuffers.GetHandle(_cbName);
+		return handle;
 	}
 
 	// 定数バッファ変数
