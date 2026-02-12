97655c19a3978abaea1c15be5f8389ad19291724
MatsudaT <matsudat.30692@gmail.com>
2026-02-12 10:59:23 +0900

    マテリアルマネージャーとモデルマネージャーを作成しました。

---
+++ 3DAction_Project/3DAction_Project/MaterialHandleManager.h
@@ -0,0 +1,71 @@
+﻿#pragma once
+
+// ==========================================
+// 【クラス概要】
+// マテリアルハンドルマネージャー
+// マテリアルのハンドルを管理するマネージャー
+// ==========================================
+
+
+// ==========================================
+// ヘッダー
+// ==========================================
+// マテリアルハンドルヘッダー
+#include "MaterialHandle.h"
+// テンプレートマネージャーヘッダー
+#include "TemplateManager.h"
+
+
+// =========================================
+// クラス
+// =========================================
+class MaterialHandleManager final
+{
+private:
+	// --------------------------------
+	// メンバー変数
+	// --------------------------------
+	TemplateManager<MaterialHandle> m_MaterialHandles;
+
+
+public:
+	// --------------------------------
+	// コンストラクタ・デストラクタ
+	// --------------------------------
+	MaterialHandleManager() = default;
+	~MaterialHandleManager() = default;
+
+	// --------------------------------
+	// マテリアルハンドル管理
+	// --------------------------------
+	Handle AddMaterialHandle(
+		const Hashed_String& name,
+		const MaterialHandle& materialHandle) {
+		return m_MaterialHandles.AddData(name, materialHandle);
+	}
+
+	// マテリアルハンドル取得
+	MaterialHandle* GetMaterialHandle(const Handle& handle) {
+		return m_MaterialHandles.GetData(handle);
+	}
+
+	// マテリアルハンドル存在確認
+	bool ExistsMaterialHandle(const Hashed_String& name) {
+		return m_MaterialHandles.Exists(name);
+	}
+
+	// マテリアルハンドル取得（名前から）
+	Handle GetMaterialHandleByName(const Hashed_String& name) {
+		return m_MaterialHandles.GetHandle(name);
+	}
+
+	// マテリアルハンドル削除
+	void ReleaseMaterialHandle(const Handle& handle) {
+		m_MaterialHandles.Remove(handle);
+	}
+
+	// マテリアルハンドル全削除
+	void ReleaseAllMaterialHandles() {
+		m_MaterialHandles.ALLClear();
+	}
+};
---
+++ 3DAction_Project/3DAction_Project/ModelHandleManager.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+
+// ==========================================
+// 【クラス概要】
+// DirectX11用モデルハンドルマネージャー
+// モデルのハンドルを管理するマネージャー
+// ==========================================
+
+
+// ==========================================
+// ヘッダー
+// ==========================================
+// モデルハンドルヘッダー
+#include "ModelHandle.h"
+// テンプレートマネージャーヘッダー
+#include "TemplateManager.h"
+
+
+// =========================================
+// クラス
+// =========================================
+class DirectX11_ModelHandleManager final
+{
+private:
+	// --------------------------------
+	// メンバー変数
+	// --------------------------------
+	TemplateManager<ModelHandle> m_ModelHandles;
+
+
+public:
+	// --------------------------------
+	// コンストラクタ・デストラクタ
+	// --------------------------------
+	DirectX11_ModelHandleManager() = default;
+	~DirectX11_ModelHandleManager() = default;
+
+
+	// --------------------------------
+	// モデルハンドル管理
+	// --------------------------------
+	Handle AddModelHandle(
+		const Hashed_String& name,
+		const ModelHandle& modelHandle) {
+		return m_ModelHandles.AddData(name, modelHandle);
+	}
+
+	// モデルハンドル取得
+	ModelHandle* GetModelHandle(const Handle& handle) {
+		return m_ModelHandles.GetData(handle);
+	}
+
+	// モデルハンドル存在確認
+	bool ExistsModelHandle(const Hashed_String& name) {
+		return m_ModelHandles.Exists(name);
+	}
+
+	// モデルハンドル取得（名前から）
+	Handle GetModelHandleByName(const Hashed_String& name) {
+		return m_ModelHandles.GetHandle(name);
+	}
+
+	// モデルハンドル削除
+	void ReleaseModelHandle(const Handle& handle) {
+		m_ModelHandles.Remove(handle);
+	}
+
+	// モデルハンドル全削除
+	void ReleaseAllModelHandle() {
+		m_ModelHandles.ALLClear();
+	}
+
+};
