4268b0586087c436f333e79bddb5751b9a639f30
MatsudaT <matsudat.30692@gmail.com>
2026-02-12 16:55:50 +0900

    SPIR-Vコンパイルの環境構築

---
+++ 3DAction_Project/3DAction_Project/Windows/0_WindowsLibrary/Windows_SPIRVCompileModule.cpp
@@ -0,0 +1,21 @@
+﻿
+// ==============================================
+// ヘッダー
+// ==============================================
+// 必須ヘッダー
+#include "Windows_SPIRVCompileModule.h"
+// ウィンドウズヘッダー
+#include <Windows.h>
+// コンパイルヘッダー
+#include <dxcapi.h>
+#pragma comment(lib, "dxcompiler.lib")
+
+
+// ==============================================
+// HLSL を SPIR-V に変換する
+// ==============================================
+bool Windows_SPIRV_CompileModule::SPIRVCompile()
+{
+
+	return true;
+}
