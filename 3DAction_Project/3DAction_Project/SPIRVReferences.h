2ff00000d4a8ccac6db991ca891247901f3172eb
MatsudaT <matsudat.30692@gmail.com>
2026-02-17 20:49:25 +0900

    リファレンス情報に必要な変数を書き出しました。

--- 3DAction_Project/3DAction_Project/SPIRVReferences.h
+++ 3DAction_Project/3DAction_Project/SPIRVReferences.h
@@ -27,6 +27,22 @@ private:
 	// SPIR-Vリフレクション情報の拡張子
 	const String kSPIRVReflectionInfoExtension = u8".txt";
 
+	// -----------------------------------
+	// 書き出し用
+	// -----------------------------------
+	// シェーダー名
+	const String kShaderName = u8"ShaderName : ";
+
+	// 入力レイアウト
+	const String kILName = u8"ILName : ";
+	const String kILIndex = u8"ILIndex : ";
+	const String kILFormat = u8"ILFormat : ";
+
+	// 定数バッファ
+	const String kCBName = u8"CBName : ";
+	const String kRegisterNumber = u8"RegisterNumber : ";
+	const String kCBSize = u8"CBSize : ";
+
 
 public:
 	// ----------------------------------------
